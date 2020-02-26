f3b61a2443c3c241598aac9265db1dc048544b2a
Isaac Pincus <zacky47222@gmail.com>
2020-02-26 20:47:48 +1100

    Add files via upload

--- gamblers.py
+++ gamblers.py
@@ -1,24 +1,24 @@
 import numpy as np
 
-casino_cash = 100000
-no_gamblers = 1000
-gamblers_cash = 100
 odds = 0.001
-gamblers = np.ones([no_gamblers,1])*gamblers_cash
 casino_wins = 0
 iterations = 1000
 # extra payout, >1 good for gamblers, <1 good for casino
-margin = 2
+margin = 1.5
 
 for i in range(iterations):
+    casino_cash = 100000
+    no_gamblers = 1000
+    gamblers_cash = 100
+    gamblers = np.ones([no_gamblers,1])*gamblers_cash
     while True:
     ##    print(str(casino_cash))
     ##    print(str(np.count_nonzero(gamblers)))
         for item, gamer in enumerate(gamblers):
             if gamer != 0:
                 # always bet 10% of their holdings
-                bet = int(np.ceil(gamer/10))
-                payout = round(bet*margin/odds)
+                bet = int(np.ceil(gamer/100))
+                payout = round((bet*margin)/odds)
                 gamblers[item] = gamblers[item] - bet
                 casino_cash = casino_cash + bet
                 if np.random.randint(1/odds) == 0:
